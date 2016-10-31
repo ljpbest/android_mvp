68ceb273b3f97d102527a4239317e8475ca77d18
ljpbest <ljpbest@qq.com>
2016-10-31 14:14:22 +0800

    这是一个MVP模式

---
+++ app/src/main/java/ljp/qianfeng/com/day_mvp_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package ljp.qianfeng.com.day_mvp_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //这是mvp
+    }
+}
