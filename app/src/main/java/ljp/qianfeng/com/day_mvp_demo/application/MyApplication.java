bf668810ffa951a5fdc3006da1b8cf8c006b138c
ljpbest <ljpbest@qq.com>
2016-10-31 14:25:13 +0800

    这是一个MVP模式

---
+++ app/src/main/java/ljp/qianfeng/com/day_mvp_demo/application/MyApplication.java
@@ -0,0 +1,19 @@
+package ljp.qianfeng.com.day_mvp_demo.application;
+
+import android.app.Application;
+
+import org.xutils.BuildConfig;
+import org.xutils.x;
+
+/**
+ * Created by Administrator on 2016/10/31 0031.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);
+        x.Ext.setDebug(BuildConfig.DEBUG);
+
+    }
+}
---
+++ app/src/main/java/ljp/qianfeng/com/day_mvp_demo/model/MainModel.java
@@ -0,0 +1,7 @@
+package ljp.qianfeng.com.day_mvp_demo.model;
+
+/**
+ * Created by Administrator on 2016/10/31 0031.
+ */
+public class MainModel {
+}
